2c104bccff424a50affe3694fe3027228152b493
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-19 14:39:26 +0530

    initial commit

---
+++ src/main/java/io/github/sarthaka24/reactiveneo4j/ReactiveNeo4jApplication.java
@@ -0,0 +1,13 @@
+package io.github.sarthaka24.reactiveneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveNeo4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sarthaka24/reactiveneo4j/ReactiveNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sarthaka24.reactiveneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveNeo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
