2cb6725beba07544711b2c5eb6275594b929135f
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-19 15:08:19 +0530

    updated import statement for StudentRepository

--- src/main/java/io/github/sarthaka24/reactiveneo4j/repository/StudentRepository.java
+++ src/main/java/io/github/sarthaka24/reactiveneo4j/repository/StudentRepository.java
@@ -1,7 +1,7 @@
 package io.github.sarthaka24.reactiveneo4j.repository;
 
 import io.github.sarthaka24.reactiveneo4j.domain.Student;
-import org.neo4j.springframework.data.repository.ReactiveNeo4jRepository;
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
 
 public interface StudentRepository extends ReactiveNeo4jRepository<Student, Long> {
 
