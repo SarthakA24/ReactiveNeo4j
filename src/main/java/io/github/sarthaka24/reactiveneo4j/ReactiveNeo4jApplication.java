c186148067dbdb7b27190b721ec0cea982a68449
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-19 15:07:59 +0530

    added @EnableNeo4jAuditing annotation

--- src/main/java/io/github/sarthaka24/reactiveneo4j/ReactiveNeo4jApplication.java
+++ src/main/java/io/github/sarthaka24/reactiveneo4j/ReactiveNeo4jApplication.java
@@ -1,9 +1,11 @@
 package io.github.sarthaka24.reactiveneo4j;
 
+import org.neo4j.springframework.data.config.EnableNeo4jAuditing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableNeo4jAuditing
 public class ReactiveNeo4jApplication {
 
     public static void main(String[] args) {
