e659e53b67e693860e45747c83878e5e01c48628
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2023-03-26 23:39:27 +0530

    removed exceptions

--- src/main/java/io/github/sarthaka24/reactiveneo4j/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.sarthaka24.reactiveneo4j.exception;
-
-public class StudentNotFoundException extends Exception {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
